623dd5b2b4b9077b7c186b1832b3c90f3da3d537
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-06-30 22:09:58 -0700

    added pages folders and files

---
+++ client/src/pages/Save/Save.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Save = _ => {
+  return (
+    <h1>This is the Save Page</h1>
+  )
+}
+
+export default Save
---
+++ client/src/pages/Search/Search.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Search = _ => {
+  return (
+    <h1>This is the Search Page</h1>
+  )
+}
+
+export default Search
