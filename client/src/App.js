0776d57aae7a8bab5f594d16dd31a6ecf5935390
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-07-11 16:03:42 -0700

    moved search states and functions to search page

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,10 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import axios from 'axios'
-import SearchBox from './components/SearchBox'
 import NavBar from './components/NavBar'
+import Search from './pages/Search'
 class App extends React.Component {
-  state = {
-    searchTerm: '',
-    title: '',
-    author: '',
-    description: '',
-    image: '',
-    link: '',
-    book: {
-      title: '',
-      author: '',
-      description: '',
-      image: '',
-      link: ''
-    }
-  }
-// This function will search for the book using the Google Books API
-  handleSearchBook = searchTerm => {
-    console.log("search term fam: ", searchTerm)
-    axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=AIzaSyAGDbzsUMeCa0dq4ANvOuWeZACpiZkSbpY`)
-    .then(response => {
-      const {
-        title,
-        authors,
-        imageLinks,
-        infoLink
-      } = response.data.items[0].volumeInfo
-      this.setState({ title, author: authors[0], image: imageLinks.smallThumbnail, link: infoLink })
-    })
-    .catch(error => {
-      console.log(error)
-    })
-
-  }
-
-
-  // componentDidMount () {
-  //   this.searchTerm = 'Harry Potter'
-  //   this.handleSearchBook('Harry Potter and the Chamber of Secrets')
-  // }
-    // when talking to server, make sure to have a url like this:
-    // http://localhost:4000
+  // when talking to server, make sure to have a url like this:
+  // http://localhost:4000
 
   //   return axios.get('http://localhost:4000/search')
   //     .then(data => {
@@ -55,14 +15,8 @@ class App extends React.Component {
   render () {
     return (
       <>
-      <NavBar/>
-      <SearchBox 
-      clickHandler={this.handleSearchBook}
-      />
-      <h1>{this.state.title}</h1>
-      <h1>{this.state.author}</h1>
-      <img src={this.state.image}></img>
-      <h1>{this.state.link}</h1>
+        <NavBar />
+        <Search />
       </>
     )
   }
--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -1,9 +1,55 @@
 import React from 'react'
+import axios from 'axios'
+import SearchBox from '../../components/SearchBox'
 
-const Search = _ => {
-  return (
-    <h1>This is the Search Page</h1>
-  )
+class Search extends React.Component {
+  state = {
+    searchTerm: '',
+    title: '',
+    author: '',
+    description: '',
+    image: '',
+    link: '',
+    book: {
+      title: '',
+      author: '',
+      description: '',
+      image: '',
+      link: ''
+    }
+  }
+  // This function will search for the book using the Google Books API
+  handleSearchBook = searchTerm => {
+    console.log("search term fam: ", searchTerm)
+    axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=AIzaSyAGDbzsUMeCa0dq4ANvOuWeZACpiZkSbpY`)
+      .then(response => {
+        const {
+          title,
+          authors,
+          imageLinks,
+          infoLink
+        } = response.data.items[0].volumeInfo
+        this.setState({ title, author: authors[0], image: imageLinks.smallThumbnail, link: infoLink })
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
+  }
+
+  render() {
+    return (
+      <>
+        <SearchBox
+          clickHandler={this.handleSearchBook}
+        />
+        <h1>{this.state.title}</h1>
+        <h1>{this.state.author}</h1>
+        <img src={this.state.image}></img>
+        <h1>{this.state.link}</h1>
+      </>
+    )
+  }
 }
 
 export default Search
