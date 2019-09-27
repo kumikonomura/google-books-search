de20293ce09896240f7b52c2be321553c082f36a
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-09-27 15:47:21 -0700

    added domain to frontend api endpoint

--- client/src/utils/Book.js
+++ client/src/utils/Book.js
@@ -4,7 +4,7 @@ const Books = {
   // GET saved books from the db
   getAllBooks: _ => axios.get("/books"),
   // POST a new book to db
-  saveBook: book => axios.post("/books", book),
+  saveBook: book => axios.post("http://localhost:4000/books", book),
   // DELETE a book from db
   deleteBook: id => axios.delete(`/books/${id}`)
 };
